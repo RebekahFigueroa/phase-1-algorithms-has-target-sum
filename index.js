2680a105922f04ba4a876faebb4ea6303115c74e
Rebekah <figueroarebekah@gmail.com>
2023-01-11 15:30:59 -0600

    commit tree db639baea2e46f3af56d60801ef8cd6ea9eda1d6

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+	// Write your algorithm here
+	const arrayMap = {}; // {}
+	for (arrayValue of array) {
+		//2
+		console.log(arrayMap);
+		console.log(arrayMap[arrayValue]);
+		const targetMinusValue = target - arrayValue; // 4-2 = 2
+		if (arrayMap[arrayValue] !== undefined) {
+			//
+			return true;
+		} else {
+			arrayMap[targetMinusValue] = arrayValue; // 2:2
+		}
+	}
+	return false;
 }
 
 /* 
@@ -9,26 +23,27 @@ function hasTargetSum(array, target) {
 /* 
   Add your pseudocode here
 */
-
+// sort array from least to greatest (array.sort((a,b) => a-b))
+// binary search
 /*
   Add written explanation of your solution here
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+	// add your own custom tests in here
+	console.log("Expecting: true");
+	console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+	console.log("Expecting: true");
+	console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+	console.log("Expecting: false");
+	console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
